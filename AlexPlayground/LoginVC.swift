aaab307dd613e25b625d8253d557e57fdf98afc8
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-02-18 11:15:47 -0800

    login works

--- AlexPlayground/LoginVC.swift
+++ AlexPlayground/LoginVC.swift
@@ -9,11 +9,48 @@ import UIKit
 import Firebase
 
 class LoginVC: UIViewController {
+    
+    public var email: String = ""
+    public var password: String = ""
 
+    
+    @IBOutlet var emailTF: UITextField!
+    
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func checkString(string: String?) -> Bool {
+        if string != nil {
+            if string!.contains("@") && string!.contains(".") {
+                return true
+            }
+        }
+        return false
+    }
 
-
+    @IBAction func loginClicked(_ sender: UIButton) {
+        if checkString(string: emailTF.text) && passwordTF.text != nil {
+            email = emailTF.text!
+            password = passwordTF.text!
+            login()
+        }
+    }
+    
+    private func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                // Could not sign in
+                print("Error signing in: " + error!.localizedDescription)
+            } else {
+                //Signed in sucessfully
+                print("Sucessfully signed in")
+            }
+        }
+    }
+    
 }
