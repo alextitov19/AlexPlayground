11c27996133f71d6c8080c45b58f928b04d0b980
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-02-18 10:56:01 -0800

    added login ui

---
+++ AlexPlayground/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  AlexPlayground
+//
+//  Created by Alex Titov on 2/18/21.
+//
+
+import UIKit
+import Firebase
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
