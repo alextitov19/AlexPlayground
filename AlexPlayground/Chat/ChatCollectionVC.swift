d199c95685649ad4b58a5a9348c26d7797e45fe7
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-02-20 23:00:07 -0800

    added chat ui

---
+++ AlexPlayground/Chat/ChatCollectionVC.swift
@@ -0,0 +1,13 @@
+//
+//  ChatCollectionVC.swift
+//  AlexPlayground
+//
+//  Created by Alex Titov on 2/20/21.
+//
+
+import UIKit
+import Firebase
+
+class ChatCollectionVC: UIViewController {
+
+}
